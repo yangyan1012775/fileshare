1dfdb9e1469d2789c92420025c577351bb76f15a
Ccroo <Ccrool@163.com>
2017-12-17 20:47:57 +0800

    修改

--- src/db/basic.ts
+++ src/db/basic.ts
@@ -8,14 +8,10 @@ const init = (db: any) => {
     password: process.env.MYSQL_PASSWORD,
     user: process.env.MYSQL_USERNAME,
   };
-    
+
   const con = mysql.createConnection(options);
   return new Promise((resolve, reject) => {
     con.connect(cbFunc(() => {
-<<<<<<< HEAD
-=======
-
->>>>>>> 5acc6ee1796b00fe105d9c0e06a3e408070b0e50
       resolve(con);
     }));
   });

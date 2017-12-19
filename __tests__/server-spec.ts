982a38aee2db57902897ab1b004a096912fb8b1b
sikaisland <1450965245@qq.com>
2017-12-19 19:06:35 +0800

    “hot数据读取”

--- __tests__/server-spec.ts
+++ __tests__/server-spec.ts
@@ -18,6 +18,24 @@ test('Should greet with message', () => {
   expect(server.server).toBe(express2);
 });
 
+test('首页url测试', done => {
+  request(app)
+    .get('/')
+    .expect(200, function(err, res) {
+      expect(err).toBeFalsy();
+      expect(res.text.includes('index')).toBeTruthy();
+      done();
+    });
+});
+test('热门文件url测试', done => {
+  request(app)
+    .get('/hots/video')
+    .expect(200, function(err, res) {
+      expect(err).toBeFalsy();
+      expect(res.text.includes('video')).toBeTruthy();
+      done();
+    });
+});
 test('测试访问用户页面success', done => {
   request(app)
     .get('/user/5555')

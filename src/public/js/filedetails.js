53b15238e6adc5587204301daa87de15c964d983
yangyan <yangyan1012@163.com>
2017-12-21 10:55:44 +0800

    删除注释

--- src/public/js/filedetails.js
+++ src/public/js/filedetails.js
@@ -1,7 +1,5 @@
 $(function () {
   var fileId = window.location.href.substr(window.location.href.length - 1, 1)
-  console.log(fileId);
-  //window.localStorage.getItem('fileId',fileId);
   $.ajax({
     url: "/api/files/:id",    //请求的url地址
     dataType: "json",   //返回格式为json
@@ -10,8 +8,6 @@ $(function () {
     type: "post",   //请求方式
     success: function (data) {
       //请求成功时处理
-      console.log(data);
-      //newfiledetails(data,'.details');
       var newName = $('<td>' + data[0].filename + '</td>');
       var newType = $('<td>' + data[0].type + '</td>');
       var newSize = $('<td>' + data[0].size + '</td>');
@@ -28,18 +24,3 @@ $(function () {
     window.location.href = "/user/download?id=" + fileId;
   });
 });
-
-// $(function () {
-//   $('.download').on('click', function () {
-  //   $.ajax({
-  //     url: '/user/:id',
-  //     type: 'get',
-  //     success: function (data) {
-  //       console.log('hello');
-  //     },
-  //     error: function (err) {
-  //       alert('数据库查询失败');
-  //     }
-  //   });
-//   });
-// })
